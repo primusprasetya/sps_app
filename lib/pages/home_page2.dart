bedf535a944ed862288d6b648ea4e68fb2c2f4c0
primusprasetya <muh.primus90@gmail.com>
2022-01-13 00:44:12 +0800

    menambahkan logs dalam database

--- lib/pages/home_page2.dart
+++ lib/pages/home_page2.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:sps_app/pages/scan_page.dart';
+// import 'package:sps_app/pages/scan_page.dart';
 import 'package:sps_app/theme.dart';
 
 class HomePage2 extends StatefulWidget {
@@ -15,9 +15,10 @@ class HomePage2 extends StatefulWidget {
 }
 
 class _HomePage2State extends State<HomePage2> {
+   TextEditingController platnomorController = TextEditingController (text: '');
   // get children => null;
-  String _displayText = 'Results go herex';
   final _database = FirebaseDatabase.instance.reference();
+  // String _displayText = 'Results go herex';
   late StreamSubscription _dailySpecialStream;
 
   // slot parkir lantai 1
@@ -39,32 +40,35 @@ class _HomePage2State extends State<HomePage2> {
 
   @override
   void initState(){
+    print('123');
     super.initState();
+
     _activateListeners();
   }
+  
   void _activateListeners(){
     _dailySpecialStream =
-     _database.child('serverSatu/slotParkir').onValue.listen((event) {
+     _database.child('server').onValue.listen((event) {
       final data = Map<String, dynamic>.from(event.snapshot.value);
       setState(() {
 
         // slot parkir lantai 1
-        statusA1 = data['A1'] as int;
-        statusA2 = data['A2'] as int;
-        statusA3 = data['A3'] as int;
-        statusA4 = data['A4'] as int;
-        statusA5 = data['A5'] as int;
+        statusA1 = data['server1']['slotParkir']['A1'] as int;
+        statusA2 = data['server1']['slotParkir']['A2'] as int;
+        statusA3 = data['server1']['slotParkir']['A3'] as int;
+        statusA4 = data['server1']['slotParkir']['A4'] as int;
+        statusA5 = data['server1']['slotParkir']['A5'] as int;
 
         // slot parkir lantai 2
-        statusB1 = data['B1'] as int;
-        statusB2 = data['B2'] as int;
-        statusB3 = data['B3'] as int;
-        statusB4 = data['B4'] as int;
-        statusB5 = data['B5'] as int;
-        statusB6 = data['B6'] as int;
-        statusB7 = data['B7'] as int;
-        _displayText = 'Slot Parkir A1 : $statusA1 \n Slot Parkir A2 : $statusA2';
-        print(data['A1']);
+        statusB1 = data['server2']['slotParkir']['B2'] as int;
+        statusB2 = data['server2']['slotParkir']['B3'] as int;
+        statusB3 = data['server2']['slotParkir']['B4'] as int;
+        statusB4 = data['server2']['slotParkir']['B5'] as int;
+        statusB5 = data['server2']['slotParkir']['B6'] as int;
+        statusB6 = data['server2']['slotParkir']['B7'] as int;
+        statusB7 = data['server2']['slotParkir']['B8'] as int;
+        // _displayText = 'Slot Parkir A1 : $statusA1 \n Slot Parkir A2 : $statusA2';
+        
       });
     });
   }
@@ -191,108 +195,7 @@ class _HomePage2State extends State<HomePage2> {
               ));
     }
 
-    Future<void> formPlat() {
-      return showDialog(
-          context: context,
-          builder: (BuildContext context) => Container(
-                // margin: EdgeInsets.zero,
-                width: MediaQuery.of(context).size.width - (2 * 3.0),
-                child: AlertDialog(
-                  backgroundColor: Color(0xff16727A),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  content: SingleChildScrollView(
-                    child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                            child: Icon(
-                              Icons.close,
-                              color: Color(0xff7EBABF),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        // const SizedBox(height: 12),
-                        Container(
-                          // width: 66.0,
-                          height: 50.0,
-
-                          decoration: BoxDecoration(
-                            color: Color(0xff1A8993),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Center(
-                              child: Row(
-                            children: [
-                              SizedBox(
-                                width: 16,
-                              ),
-                              Expanded(
-                                child: TextFormField(
-                                  style: subtitleStyle.copyWith(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                  decoration: InputDecoration.collapsed(
-                                    hintText: 'Plat Nomor',
-                                    hintStyle: subtitleStyle.copyWith(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          )),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          'silahkan masukan nomor plat anda',
-                          style: subtitleStyle.copyWith(
-                            fontSize: 12,
-                            letterSpacing: 1.5,
-                            color: Color(0xffffffff),
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        const SizedBox(height: 24),
-                        Container(
-                          width: 150,
-                          height: 40,
-                          margin: EdgeInsets.zero,
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                                // Navigator.push(
-                                //   context,
-                                //   MaterialPageRoute(builder: (context) => const ScanPage()),
-                                // );                  
-                              
-                                          
-                            },
-                            style: TextButton.styleFrom(
-                              backgroundColor: Color(0xff1A8993),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                            child: Text('Input', style: subtitleStyle),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ));
-    }
-
+   
     Future<void> petunjukParkiran() {
       return showDialog(
           context: context,
@@ -429,10 +332,124 @@ class _HomePage2State extends State<HomePage2> {
               ));
     }
 
+     Future<void> formPlat() {
+      return showDialog(
+         context: context,
+          builder: (BuildContext context) => Container(
+                // margin: EdgeInsets.zero,
+                width: MediaQuery.of(context).size.width - (2 * 3.0),
+                child: AlertDialog(
+                  backgroundColor: Color(0xff16727A),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  content: SingleChildScrollView(
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Icon(
+                              Icons.close,
+                              color: Color(0xff7EBABF),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        // const SizedBox(height: 12),
+                        Container(
+                          // width: 66.0,
+                          height: 50.0,
+
+                          decoration: BoxDecoration(
+                            color: Color(0xff1A8993),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Center(
+                              child: Row(
+                            children: [
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Expanded(
+                                child: TextFormField(
+                                  style: subtitleStyle.copyWith(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  controller: platnomorController,
+                                  decoration: InputDecoration.collapsed(
+                                    hintText: 'Plat Nomor',
+                                    hintStyle: subtitleStyle.copyWith(
+                                      color: Color(0XFF296C72),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          'silahkan masukan nomor plat anda',
+                          style: subtitleStyle.copyWith(
+                            fontSize: 12,
+                            letterSpacing: 1.5,
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        Container(
+                          width: 150,
+                          height: 40,
+                          margin: EdgeInsets.zero,
+                          child: TextButton(
+                            onPressed: () {
+                             final logRef = <String, dynamic>{
+                               'Plat Kendaraan': platnomorController.text,
+                              //  'time': DateTime.now().millisecondsSinceEpoch 
+                               'time': DateTime.now().toString()
+                             };
+                             print(platnomorController.text);
+                             _database
+                             .child('logs')
+                             .push()
+                             .set(logRef)
+                             .then((_) => print('berhasil menginput plat nomor'))
+                             .catchError((error)=> print('terjadi error $error'));
+                              Navigator.pop(context);
+                              // Navigator.push(
+                              //   context,
+                              //   MaterialPageRoute(
+                              //       builder: (context) => const ScanPage()),
+                              // );
+                            },
+                            style: TextButton.styleFrom(
+                              backgroundColor: Color(0xff1FA2AD),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: Text('Input', style: subtitleStyle),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+    }
     Widget slotParkir(String kode, int status) {
       return InkWell(
         onTap: () {
-          formPlat();
+          // formPlat();
           // showConfirmDialog();
           //  qrcodePOP();
           // petunjukParkiran();
@@ -637,11 +654,6 @@ class _HomePage2State extends State<HomePage2> {
               child: InkWell(
                 onTap: () {
                   formPlat();
-
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ScanPage()),
-                  );
                 },
                 child: Container(
                   child: SvgPicture.asset(
@@ -666,4 +678,11 @@ class _HomePage2State extends State<HomePage2> {
       ),
     );
   }
+
+  //   @override
+  // void deactivate(){
+  //   _dailySpecialStream.cancel();
+  //   super.deactivate();
+  // }
+
 }
--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -5,51 +5,41 @@ import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:sps_app/theme.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-
 class ScanPage extends StatefulWidget {
-  const ScanPage({ Key? key }) : super(key: key);
+  const ScanPage({Key? key}) : super(key: key);
 
   @override
   _ScanPageState createState() => _ScanPageState();
 }
 
 class _ScanPageState extends State<ScanPage> {
-  
+  TextEditingController platnomorController = TextEditingController (text: '');
+
+
   Barcode? qr;
   QRViewController? qrController;
   final _database = FirebaseDatabase.instance.reference();
   late StreamSubscription _dailySpecialStream;
 
-  void updateData(){
-   _dailySpecialStream =
-    _database.child('serverSatu/slotParkir').onValue.listen((event) {
+  void updateData() {
+    var cek = true;
+    _dailySpecialStream = _database.child('server').onValue.listen((event) {
       final data = Map<String, dynamic>.from(event.snapshot.value);
 
       print(data);
-     
-     late var cek = true;
-      data.forEach((index, value){
-        if(cek){
-          if(value == 0){
-            data[index] = 2;
+
+      print("SERVER");
+      data.forEach((indexServer, valueServer) {
+        valueServer['slotParkir'].forEach((indexSlot, valueSlot) {
+          if (valueSlot == 0 && cek) {
+            _database
+                .child('server/' + indexServer + '/slotParkir')
+                .update({indexSlot: 2});
             cek = false;
           }
-        }
+        });
       });
-      print(data);
-      print(cek);
-    _database
-    .child('serverSatu/slotParkir')
-    // .child('serverDua/slotParkir')
-    // .push()
-    .update(data);
     });
-
-    /* for (initial value; termination_condition; step){
-      statements
-    }
-     */
-    
   }
 
   final qrKey = GlobalKey(debugLabel: 'QR');
@@ -62,6 +52,119 @@ class _ScanPageState extends State<ScanPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Future<void> formPlat() {
+    //   return showDialog(
+    //      context: context,
+    //       builder: (BuildContext context) => Container(
+    //             // margin: EdgeInsets.zero,
+    //             width: MediaQuery.of(context).size.width - (2 * 3.0),
+    //             child: AlertDialog(
+    //               backgroundColor: Color(0xff16727A),
+    //               shape: RoundedRectangleBorder(
+    //                 borderRadius: BorderRadius.circular(12),
+    //               ),
+    //               content: SingleChildScrollView(
+    //                 child: Column(
+    //                   // mainAxisAlignment: MainAxisAlignment.center,
+    //                   children: [
+    //                     Align(
+    //                       alignment: Alignment.centerLeft,
+    //                       child: InkWell(
+    //                         onTap: () {
+    //                           Navigator.pop(context);
+    //                         },
+    //                         child: Icon(
+    //                           Icons.close,
+    //                           color: Color(0xff7EBABF),
+    //                         ),
+    //                       ),
+    //                     ),
+    //                     const SizedBox(height: 20),
+    //                     // const SizedBox(height: 12),
+    //                     Container(
+    //                       // width: 66.0,
+    //                       height: 50.0,
+    //                       decoration: BoxDecoration(
+    //                         color: Color(0xff1A8993),
+    //                         borderRadius: BorderRadius.circular(12),
+    //                       ),
+    //                       child: Center(
+    //                           child: Row(
+    //                         children: [
+    //                           SizedBox(
+    //                             width: 16,
+    //                           ),
+    //                           Expanded(
+    //                             child: TextFormField(
+    //                               style: subtitleStyle.copyWith(
+    //                                 fontSize: 12,
+    //                                 fontWeight: FontWeight.w500,
+    //                               ),
+    //                               controller: platnomorController,
+    //                               decoration: InputDecoration.collapsed(
+    //                                 hintText: 'Plat Nomor',
+    //                                 hintStyle: subtitleStyle.copyWith(
+    //                                   color: Color(0XFF296C72),
+    //                                   fontSize: 12,
+    //                                   fontWeight: FontWeight.w500,
+    //                                 ),
+    //                               ),
+    //                             ),
+    //                           )
+    //                         ],
+    //                       )),
+    //                     ),
+    //                     const SizedBox(height: 12),
+    //                     Text(
+    //                       'silahkan masukan nomor plat anda',
+    //                       style: subtitleStyle.copyWith(
+    //                         fontSize: 12,
+    //                         letterSpacing: 1.5,
+    //                         color: Color(0xffffffff),
+    //                         fontWeight: FontWeight.w400,
+    //                       ),
+    //                     ),
+    //                     const SizedBox(height: 24),
+    //                     Container(
+    //                       width: 150,
+    //                       height: 40,
+    //                       margin: EdgeInsets.zero,
+    //                       child: TextButton(
+    //                         onPressed: () async{
+    //                          final logRef = <String, dynamic>{
+    //                            'Plat Kendaraan': platnomorController,
+    //                            'time': DateTime.now().millisecondsSinceEpoch 
+    //                          };
+    //                          print(platnomorController.text);
+    //                          _database
+    //                          .child('log')
+    //                          .push()
+    //                          .set(logRef)
+    //                          .then((_) => print('berhasil menginput plat nomor'))
+    //                          .catchError((error)=> print('terjadi error $error'));
+    //                           Navigator.pop(context);
+    //                           Navigator.push(
+    //                             context,
+    //                             MaterialPageRoute(
+    //                                 builder: (context) => const ScanPage()),
+    //                           );
+    //                         },
+    //                         style: TextButton.styleFrom(
+    //                           backgroundColor: Color(0xff1FA2AD),
+    //                           shape: RoundedRectangleBorder(
+    //                             borderRadius: BorderRadius.circular(12),
+    //                           ),
+    //                         ),
+    //                         child: Text('Input', style: subtitleStyle),
+    //                       ),
+    //                     ),
+    //                   ],
+    //                 ),
+    //               ),
+    //             ),
+    //           ));
+    // }
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -75,16 +178,13 @@ class _ScanPageState extends State<ScanPage> {
         ),
         elevation: 0,
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          qrCodeView(context),
-          Positioned(
-            bottom: 30,
-            child: resultQr(),
-          ),
-        ]
-      ),
+      body: Stack(alignment: Alignment.center, children: <Widget>[
+        qrCodeView(context),
+        Positioned(
+          bottom: 30,
+          child: resultQr(),
+        ),
+      ]),
     );
   }
 
@@ -103,16 +203,16 @@ class _ScanPageState extends State<ScanPage> {
   }
 
   Widget qrCodeView(BuildContext context) => QRView(
-    key: qrKey, 
-    onQRViewCreated: qrCodeViewCreated,
-    overlay: QrScannerOverlayShape(
-      borderWidth: 10,
-      borderLength: 20,
-      borderRadius: 12,
-      borderColor: primaryColor,
-      cutOutSize: MediaQuery.of(context).size.width * 0.8,
-    ),
-  );
+        key: qrKey,
+        onQRViewCreated: qrCodeViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderWidth: 10,
+          borderLength: 20,
+          borderRadius: 12,
+          borderColor: primaryColor,
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+        ),
+      );
 
   void qrCodeViewCreated(QRViewController qrController) {
     setState(() {
@@ -124,11 +224,9 @@ class _ScanPageState extends State<ScanPage> {
         this.qr = qr;
         qrController.stopCamera();
       });
-      if(qr != null){
+      if (qr != null) {
         updateData();
       }
     });
-
   }
-
 }
